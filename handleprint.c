993d14c60f7d168675685a0121857dbcbd14d973
AngieKinga <musanga1911@gmail.com>
2023-03-26 22:49:39 -0700

    printf

--- handleprint.c
+++ handleprint.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
  * handle_print - Prints an argument based on its type
- * @fmt: Formatted string in which to print the arguments
- * @size: size specifier
- * @list: List of arguments to be printed
- * @buffer: Buffer array to handle print
+ * @fmt: Formatted string in which to print the arguments.
+ * @list: List of arguments to be printed.
+ * @ind: ind.
+ * @buffer: Buffer array to handle print.
  * @flags: Calculates active flags
- * @width: get width
- * @ind: ind
+ * @width: get width.
  * @precision: Precision specification
+ * @size: Size specifier
  * Return: 1 or 2;
  */
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
@@ -47,4 +47,3 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	}
 	return (printed_chars);
 }
-
